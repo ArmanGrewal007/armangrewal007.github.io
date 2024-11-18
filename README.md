80e77c8552d551fbf077dba8e44b272ec6caeb27
ArmanGrewal007 <armansinghgrewal@gmail.com>
2024-11-19 03:00:45 +0530

    smol changes

---
+++ README.md
@@ -0,0 +1,6 @@
+# Steps
+
+```bash
+npm init -y
+npm install animejs babel-plugin-styled-components gatsby-plugin-google-analytics gatsby-plugin-image gatsby-plugin-manifest gatsby-plugin-netlify gatsby-plugin-offline gatsby-plugin-react-helmet gatsby-plugin-robots-txt gatsby-plugin-sharp gatsby-plugin-sitemap gatsby-plugin-styled-components gatsby-remark-external-links gatsby-remark-images gatsby-remark-prismjs gatsby-source-filesystem gatsby-transformer-remark gatsby-transformer-sharp lodash prismjs prop-types react react-dom react-helmet react-transition-group scrollreveal styled-components gatsby-remark-code-titles
+```
--- src/components/footer.js
+++ src/components/footer.js
@@ -77,7 +77,7 @@ const Footer = () => {
     if (process.env.NODE_ENV !== 'production') {
       return;
     }
-    fetch('https://api.github.com/repos/bchiang7/v4')
+    fetch('https://api.github.com/repos/armangrewal007/v4')
       .then(response => response.json())
       .then(json => {
         const { stargazers_count, forks_count } = json;
@@ -105,7 +105,7 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
+        <a href="https://github.com/armangrewal007/v4">
           <div>Built with ♥️ by <u><b>ArmanGrewal007</b></u>
           </div>
 
