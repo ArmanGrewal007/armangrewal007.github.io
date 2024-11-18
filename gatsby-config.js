08826bba817743416a62145b38cbb4e853e333aa
ArmanGrewal007 <armansinghgrewal@gmail.com>
2024-11-19 04:10:40 +0530

    removed desc

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteMetadata: {
     title: 'Arman Grewal',
     description:
-      'Arman Grewal is an IIM alumnus who spearheads marketing initiatives at Tata Steel and is also a freelance tech blogger.',
+      '',
     siteUrl: 'https://armangrewal007.github.io', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@armangrewal007',
