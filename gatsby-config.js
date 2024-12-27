1e69d4583c77e9f19aa76e1d2a20e333b72992fe
ArmanGrewal007 <armansinghgrewal@gmail.com>
2024-12-27 13:11:44 +0530

    Updated tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -180,7 +180,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-45666519-2',
+        trackingId: 'G-Q73ENGPMH2',
       },
     },
   ],
